6f80eda8715f880ac0531afb8a1bda3c89f2cafb
NoamRabbani <rabbani.noam@gmail.com>
2019-07-22 14:21:09 -0400

    Added step_functions.r

--- scripts/analysis/step_functions.r
+++ scripts/analysis/step_functions.r
@@ -4,7 +4,7 @@ require("rms")
 
 issues = read.csv("dataset/hbase_features_imputed.csv", header = TRUE, sep="\t")
 
-# transform columns into factors with the modes as first value
+# Apply right data types to columns.
 issues$start <- as.numeric(issues$start)
 issues$end <- as.numeric(issues$end)
 issues$priority <- factor(issues$priority)
@@ -20,36 +20,36 @@ issues$has_fix_change <- as.numeric(issues$has_fix_change)
 issues$reporter_rep <- as.numeric(issues$reporter_rep)
 issues$assignee_workload <- as.numeric(issues$assignee_workload)
 
+
+issues2 <- survSplit(Surv(start, end, is_dead) ~., data=issues, cut=c(365,730,1095), episode="tgroup")
+write.csv(issues2, "./dataset/hbase_features_tgroup.csv", quote=FALSE)
+
+# issues2$interaction_is_assigned <- issues2$is_assigned * strata(issues2$tgroup)
+issues2$interaction_comment_count <- interaction(issues2$comment_count, strata(issues2$tgroup))
+
 dd <- datadist(issues); options(datadist = "dd")
 units(start) <- "Day"
 units(end) <- "Day"
 
+# f <- cph(Surv(start, end, is_dead) ~ priority + comment_count * strat(issues2$tgroup),
+#         x=TRUE, y=TRUE, singular.ok=TRUE, data=issues2)
+# f
 
 
-f <- cph(Surv(start, end, is_dead) ~ priority + issuetype + is_assigned + rcs(comment_count,4) + rcs(link_count, 4) +
-        rcs(affect_count, 4) + rcs(fix_count,4) + has_priority_change + 
-        has_desc_change + rcs(has_fix_change,4) + rcs(reporter_rep,4) + rcs(assignee_workload,4),
-        x=TRUE, y=TRUE, data=issues)
+f <- cph(Surv(start, end, is_dead) ~ priority + issuetype + is_assigned + rcs(comment_count,4)*strat(tgroup) + rcs(link_count, 4) +
+        rcs(affect_count, 4)*strat(tgroup) + rcs(fix_count,4)*strat(tgroup) + rcs(has_priority_change,4)*strat(tgroup) + 
+        has_desc_change + rcs(has_fix_change,4)*strat(tgroup) + rcs(reporter_rep,4)*strat(tgroup) + rcs(assignee_workload,4),
+        x=TRUE, y=TRUE, data=issues2)
 print(f, latex = TRUE, coefs = FALSE)
 
+
 z <- predict(f, type="terms")
-f.short <- cph(Surv(start, end, is_dead) ~ z, x=TRUE, y=TRUE, data=issues)
+f.short <- cph(Surv(start, end, is_dead) ~ z, x=TRUE, y=TRUE, data=issues2)
 zph <- cox.zph(f.short, transform="identity")
 zph
+# issues_tgroup = survSplit(Surv(start, end, is_dead) ~ priority + issuetype + is_assigned + rcs(comment_count,4) + rcs(link_count, 4) +
+#         rcs(affect_count, 4) + rcs(fix_count,4) + has_priority_change + 
+#         has_desc_change + rcs(has_fix_change,4) + rcs(reporter_rep,4) + rcs(assignee_workload,4),
+#         data=issues, cut=c(1000), episode="tgroup", id="issuekey")
 
-issues_tgroup = survSplit(Surv(start, end, is_dead) ~ priority + issuetype + is_assigned + rcs(comment_count,4) + rcs(link_count, 4) +
-        rcs(affect_count, 4) + rcs(fix_count,4) + has_priority_change + 
-        has_desc_change + rcs(has_fix_change,4) + rcs(reporter_rep,4) + rcs(assignee_workload,4),
-        data=issues, cut=c(1000), episode="tgroup", id="issuekey")
-
-summary(issues_tgroup)
-
-# f <- cph(Surv(start, end, is_dead) ~ priority + issuetype + is_assigned:strata(tgroup) + rcs(comment_count,4) + rcs(link_count, 4) +
-#                     rcs(affect_count, 4) + rcs(fix_count,4) + has_priority_change + 
-#                     has_desc_change + rcs(has_fix_change,4) + rcs(reporter_rep,4) + rcs(assignee_workload,4),
-#         x=TRUE, y=TRUE, data=issues_tgroup)
-
-# z <- predict(f, type="terms")
-# f.short <- cph(Surv(start, end, is_dead) ~ z, x=TRUE, y=TRUE, data=issues_tgroup)
-# zph <- cox.zph(f.short, transform="identity")
-# zph
+# summary(issues_tgroup)
